fd2eefb54f4a327882c1331236a9640672164cfc
buddhika <scopic@Buddhikas-MacBook-Air.local>
2022-07-24 04:55:45 +0530

    Initial changes to update output priority based on the time and battery voltage

---
+++ app.js
@@ -0,0 +1,27 @@
+const sha1 = require('./lib/sha1');
+const api = require('./lib/api');
+const service = require('./lib/service');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+  try {
+    const loginData = await api.login('plbsam', 'ssakoo');
+    const summary = await api.getSummary(loginData);
+    const outputPriority = await api.getSetting(loginData, 'bse_output_source_priority');
+    const nextValue = service.getNewOutputPrioritySetting(summary['Battery Voltage'], summary['Timestamp'], outputPriority.val);
+    if (nextValue) {
+      await api.updateSetting(loginData, 'bse_output_source_priority', nextValue);
+      res.send(`value updated to ${service.valueMap[nextValue]} based on ${JSON.stringify(summary)} `);
+    }
+    res.send('no change required');
+  } catch (e) {
+    res.send(e.toString());
+  }
+})
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
