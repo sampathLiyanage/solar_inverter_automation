cf6bb734bc12c4d54366f64e2d5a48514699fac7
buddhika <buddhika.l@scopicsoftware.com>
2022-08-05 16:04:12 +0530

    adding charge discharge current based stratergy

--- index.js
+++ index.js
@@ -4,33 +4,69 @@ const service = require('./lib/service');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 
-const handleRequest = async (res) => {
+const applyChargeDischargeBasedStrategy = async (res) => {
   const loginData = await api.login('plbsam', 'ssakoo');
+  const fromTime = '07:00:00';
+  const summary = await api.getSummary(loginData);
+  const outputPriority = await api.getSetting(loginData, 'bse_output_source_priority');
+  const chargeCurrentSum = await api.getChartDataUptoNowForToday(loginData, 'bt_battery_charging_current', fromTime);
+  const dischargeCurrentSum = await api.getChartDataUptoNowForToday(loginData, 'bt_battery_discharge_current', fromTime);
+  const nextValue = service.getNewOutputPrioritySettingBasedOnChargeDischargeData(
+    summary['Battery Voltage'],
+    chargeCurrentSum,
+    dischargeCurrentSum,
+    summary['Timestamp'],
+    outputPriority.val
+  );
+  if (nextValue) {
+    await api.updateSetting(loginData, 'bse_output_source_priority', nextValue);
+    res.send({
+      currentStatus: { ...summary, chargeCurrentSum, dischargeCurrentSum, outputPriority: outputPriority?.val },
+      nextChargePriority: service.valueMap[nextValue],
+      message: `Output priority updated to ${service.valueMap[nextValue]}`
+    });
+  }
+  res.send({
+    currentStatus: { ...summary, chargeCurrentSum, dischargeCurrentSum, outputPriority: outputPriority?.val },
+    nextChargePriority: outputPriority?.val,
+    message: `No change required`
+  });
+}
+
+const applyVoltageBasedStrategy = async (res) => {
   const summary = await api.getSummary(loginData);
   const outputPriority = await api.getSetting(loginData, 'bse_output_source_priority');
   const nextValue = service.getNewOutputPrioritySetting(summary['Battery Voltage'], summary['Timestamp'], outputPriority.val);
   if (nextValue) {
     await api.updateSetting(loginData, 'bse_output_source_priority', nextValue);
-    res.send(`Output priority updated to ${service.valueMap[nextValue]} based on ${JSON.stringify({ ...summary, outputPriority: outputPriority?.val })} `);
+    res.send({
+      currentStatus: { ...summary, outputPriority: outputPriority?.val },
+      nextChargePriority: service.valueMap[nextValue],
+      message: `Output priority updated to ${service.valueMap[nextValue]}`
+    });
   }
-  res.send(`no change required based on ${JSON.stringify({ ...summary, outputPriority: outputPriority?.val })}`);
+  res.send({
+    currentStatus: { ...summary, outputPriority: outputPriority?.val },
+    nextChargePriority: outputPriority?.val,
+    message: `No change required`
+  });
 }
 
 app.get('/', async (req, res) => {
   try {
-    await handleRequest(res);
+    await applyChargeDischargeBasedStrategy(res);
   } catch (e) {
     try {
       await new Promise(r => setTimeout(r, 60000));
       console.log('first request sequence failed. Retrying...');
-      await handleRequest(res);
+      await applyChargeDischargeBasedStrategy(res);
     } catch (e) {
       try {
         await new Promise(r => setTimeout(r, 60000));
         console.log('second request sequence failed. Retrying...');
-        await handleRequest(res);
+        await applyChargeDischargeBasedStrategy(res);
       } catch (e) {
         console.log('Request sequence failed. Giving up...');
         res.send(e.toString());
