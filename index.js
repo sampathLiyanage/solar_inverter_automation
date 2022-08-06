9218dcd83e13de279a10e770548ebff551251b0d
buddhika <buddhika.l@scopicsoftware.com>
2022-08-06 20:14:57 +0530

    removing duplicate code

--- index.js
+++ index.js
@@ -23,16 +23,17 @@ const handleRequest = async (req, res) => {
     threshold1,
     threshold2
   );
+  const currentStatus = { ...summary, chargedAmpHours: chargeCurrentSum * 0.1, dischargedAmpHours: dischargeCurrentSum * 0.1 * 0.65, outputPriority: outputPriority?.val };
   if (nextValue) {
     await api.updateSetting(loginData, 'bse_output_source_priority', nextValue);
     res.send({
-      currentStatus: { ...summary, chargedAmpHours: chargeCurrentSum * 0.1, dischargedAmpHours: dischargeCurrentSum * 0.1 * 0.65, outputPriority: outputPriority?.val },
+      currentStatus,
       nextChargePriority: service.valueMap[nextValue],
       message: `Output priority updated to ${service.valueMap[nextValue]}`
     });
   }
   res.send({
-    currentStatus: { ...summary, chargedAmpHours: chargeCurrentSum * 0.1, dischargedAmpHours: dischargeCurrentSum * 0.1 * 0.65, outputPriority: outputPriority?.val },
+    currentStatus,
     nextChargePriority: outputPriority?.val,
     message: `No change required`
   });
